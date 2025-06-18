ddb65513f8cf07d8f3e83d87a2fe6424a81f46ff
Edna <50573134+ednamafukashe@users.noreply.github.com>
2025-06-18 15:17:34 +0200

    links

--- src/components/NavAlert.js
+++ src/components/NavAlert.js
@@ -6,7 +6,7 @@ export const NavAlert = () => {
     <section className="nav-alert">
       <span>
         🚀 Looking to hire a Software Developer? Please review{' '}
-        <Link to="https://www.canva.com/design/DAGqssfNTTs/Iy44d5aREsfPuOnzKLSPKg/edit?utm_content=DAGqssfNTTs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">my resume</Link> and projects. 🚀
+        <Link to="https://www.canva.com/design/DAGqtCsdgt8/FSntkdoEKrxBY9JJ_Gq47w/view?utm_content=DAGqtCsdgt8&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h3f64b87a39">my resume</Link> and projects. 🚀
       </span>
     </section>
   );
