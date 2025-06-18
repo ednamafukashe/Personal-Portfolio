2d65ff0c977cdf725f3e95c0d0f13b12a9aaf0f0
Edna <50573134+ednamafukashe@users.noreply.github.com>
2025-06-18 12:10:06 +0200

    Update airtableData.js

--- src/data/airtableData.js
+++ src/data/airtableData.js
@@ -9,7 +9,7 @@ const AIRTABLE_DATA = {
       title: '1. Gateway Stream- Hotel Booking Platform',
       desc: 'Gateway is a comprehensive hotel booking platform designed to cater to the African hospitality market, offering functionality similar to Booking.com. The platform enables users to effortlessly search, book, modify stay dates, and pay for accommodations at over 5,000 hotels. It is integrated with the HyperGuest API, ensuaring real-time availability and pricing, providing users with a seamless booking experience.',
       imgUrl:
-        'https://res.cloudinary.com/de525o0ei/image/upload/v1750240245/Screenshot_2025-06-18_112629_bei4yj.png',
+        'https://res.cloudinary.com/de525o0ei/image/upload/v1731923597/gateway.png',
       isClientWork: true,
       stack: ['Svelte','NodeJS', 'TailwindCSS', 'Vercel', 'Supabase'],
       url: 'https://www.gatewaystream.com/',
@@ -19,7 +19,7 @@ const AIRTABLE_DATA = {
       title: '2. NAMA Voting Platform',
       desc: 'Developed a dynamic voting platform for Zimbabwe’s premier music awards, the National Arts Merit Awards (NAMA). The app allowed users to nominate their favorite artists during the initial phase, after which the top five nominees advanced to a final voting round for the People’s Choice Award. The system ensured a seamless, secure, and engaging user experience throughout the nomination and voting process.',
       imgUrl:
-        'https://res.cloudinary.com/de525o0ei/image/upload/v1731927139/weather.png',
+        'https://res.cloudinary.com/de525o0ei/image/upload/v1750240245/Screenshot_2025-06-18_112629_bei4yj.png',
       isClientWork: true,
       stack: [
         'Svelte JS',
